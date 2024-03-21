7bf3faa742ccf1e409df4170cb9e68c1d68da5d5
gztin <atharsfake@gmail.com>
2024-03-21 10:04:29 +0800

    Initial Commit

---
+++ SailarBeer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SailarBeer
+//
+//  Created by 陳宗廷 on 2024/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
